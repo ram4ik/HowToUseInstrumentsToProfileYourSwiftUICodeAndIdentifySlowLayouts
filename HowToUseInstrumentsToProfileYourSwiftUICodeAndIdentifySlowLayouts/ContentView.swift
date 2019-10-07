d26d84d012050d4406fa40e624c57dc0c8894c1d
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-07 11:39:58 +0300

    init

--- HowToUseInstrumentsToProfileYourSwiftUICodeAndIdentifySlowLayouts/ContentView.swift
+++ HowToUseInstrumentsToProfileYourSwiftUICodeAndIdentifySlowLayouts/ContentView.swift
@@ -6,11 +6,37 @@
 //  Copyright © 2019 com.ri. All rights reserved.
 //
 
+import Combine
 import SwiftUI
 
+class FrequentUpdater: ObservableObject {
+    let objectWillChange = PassthroughSubject<Void, Never>()
+    var timer: Timer?
+
+    init() {
+        timer = Timer.scheduledTimer(
+            withTimeInterval: 0.01,
+            repeats: true
+        ) { _ in
+            self.objectWillChange.send()
+        }
+    }
+}
+
 struct ContentView: View {
+    @ObservedObject var updater = FrequentUpdater()
+    @State private var tapCount = 0
+
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("\(UUID().uuidString)")
+
+            Button(action: {
+                self.tapCount += 1
+            }) {
+                Text("Tap count: \(tapCount)")
+            }
+        }
     }
 }
 
